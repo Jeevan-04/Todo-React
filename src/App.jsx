6a8ff13f88a180979ec77c6573f0f21aa0cd298a
Jeevan <2023.jeevann@isu.ac.in>
2024-06-18 20:42:47 +0530

    TodoList

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css'
+import ToDoList from './ToDoList'
+
+function App() {
+
+  return (
+    <>
+      <ToDoList/>
+    </>
+  )
+}
+
+export default App
---
+++ src/ToDoList.jsx
@@ -0,0 +1,90 @@
+import { useState } from 'react'
+import styles from './ToDoList.module.css'
+
+export default function ToDoList() {
+  
+    const [tasks, setTasks] =useState([]);
+    const [newTask, setNewTask] =useState("");
+
+    function handleInputChange(event){
+        setNewTask(event.target.value)
+    }
+
+    function addTask(){
+        if (newTask.trim() !== ""){
+            setTasks(t => [...t, newTask])
+            setNewTask("");
+        }
+    }
+
+    function deleteTask(index){
+        const updatedTasks = tasks.filter((_, i)=> i !== index);
+        setTasks(updatedTasks);
+    }
+
+    function moveTaskUp(index){
+
+        if(index > 0){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = 
+            [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+
+    }
+
+    function moveTaskDown(index){
+
+        if(index < tasks.length -1){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = 
+            [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+        
+    }
+
+
+  
+    return (
+    <div className={styles.to_do_list}>
+        <h1>To-Do-List</h1>
+
+        <div>
+            <input
+                    type="text"
+                    placeholder='Enter a task...'
+                    value={newTask}
+                    onChange={handleInputChange}/>
+            <button
+                className={styles.add_button}
+                onClick={addTask}>
+                Add
+            </button>
+        </div>
+
+        <ol>
+            {tasks.map((task,index)=>
+                <li key= {index}>
+                    <span className={styles.text}>{task}</span>
+                    <button
+                        className={styles.delete_button}
+                        onClick={() => deleteTask(index)}>
+                        Delete
+                    </button>
+                    <button
+                        className={styles.move_button}
+                        onClick={() => moveTaskUp(index)}>
+                        👆
+                    </button>
+                    <button
+                        className={styles.move_button}
+                        onClick={() => moveTaskDown(index)}>
+                        👇
+                    </button>
+                </li>
+            )}
+        </ol>
+    </div>
+  )
+}
